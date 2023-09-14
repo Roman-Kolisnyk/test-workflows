053ec551c5577231403d1959a762c5a4d5ec8f5d
Roman-Kolisnyk <79081564+Roman-Kolisnyk@users.noreply.github.com>
2023-09-14 17:20:09 +0300

    Update greating.js

--- .github/actions/js-action/greating.js
+++ .github/actions/js-action/greating.js
@@ -6,9 +6,6 @@ try {
   console.log(`Hello ${userName}! How are you?`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
